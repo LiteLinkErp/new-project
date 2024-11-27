ecd08b6aee0c595385a76b197ff270a0196a19c1
LiteLinkErp <litelinkerp@gmail.com>
2024-11-28 00:36:45 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 async function fetchMessage() {
-    const response = await fetch('/api/hello');
+    const response = await fetch('https://xtremepadelzambia-ln6wiorvw-litelinkerps-projects.vercel.app/api/apex');
     const data = await response.json();
     document.getElementById('message').textContent = data.message;
 }
