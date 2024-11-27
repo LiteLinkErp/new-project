becceb4b5bdcf9828cc7175d85d0ce258d143e73
LiteLinkErp <litelinkerp@gmail.com>
2024-11-28 00:46:01 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,27 @@
 async function fetchMessage() {
-    const response = await fetch('https://xtremepadelzambia-ln6wiorvw-litelinkerps-projects.vercel.app/api/apex');
-    const data = await response.json();
-    document.getElementById('message').textContent = data.message;
+    fetch('https://xtremepadelzambia-ln6wiorvw-litelinkerps-projects.vercel.app/api/apex', {
+    method: 'GET', // or 'POST', 'PUT', 'DELETE', etc.
+    headers: {
+        'Access-Control-Allow-Origin': '*', // Or a specific origin
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Allowed methods
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allowed headers
+    },
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data); // Handle the API response data
+    })
+    .catch(error => {
+        console.error('There was an error!', error);
+    });
+
+    //document.getElementById('message').textContent = data.message;
 }
+
+
+
